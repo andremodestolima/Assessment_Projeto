05cf23461b47025e9d1411279bd733fa4e585a67
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-23 13:45:28 -0200

    v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -2,10 +2,10 @@ function pronto(){
     window.addEventListener('push', ratchetPronto);
     function ratchetPronto(){
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/index.html'){
-            alert(pag1 !!);
+            alert("pag1 !!");
         }
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/lista.html'){
-            alert(pag2 !!);
+            alert("pag2 !!");
         }
 
 
