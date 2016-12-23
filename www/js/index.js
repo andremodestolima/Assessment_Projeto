f4e9443ec21ee58c7b74c6f3c136dcd50bc85da2
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-23 14:26:27 -0200

    v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,14 @@
 function pronto(){
     window.addEventListener('push', ratchetPronto);
     var paginaAtual = "";
+    document.getElementById("hospitais").addEventListener("click", definirPagina("hospitais"), false);
+    document.getElementById("delegacias").addEventListener("click", definirPagina("delegacias"), false);
+    document.getElementById("pontosTuristicos").addEventListener("click", definirPagina("pontosTuristicos"), false);
+    document.getElementById("praias").addEventListener("click", definirPagina("praias"), false);
+    document.getElementById("ondeComer").addEventListener("click", definirPagina("ondeComer"), false);
+    document.getElementById("ondeDormir").addEventListener("click", definirPagina("ondeDormir"), false);
+    document.getElementById("eventos").addEventListener("click", definirPagina("eventos"), false);
+    document.getElementById("banheiros").addEventListener("click", definirPagina("banheiros"), false);
 
     function definirPagina(pagina){
         paginaAtual = pagina;
@@ -13,10 +21,6 @@ function pronto(){
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/lista.html'){
             alert(paginaAtual);
         }
-
-
-
-
 
     }
 }
