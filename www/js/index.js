1871b03404350c64b6012e8bb6597c0371cdf4c4
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-23 17:35:58 -0200

    v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,6 @@ function pronto(){
     window.addEventListener('push', ratchetPronto);
     window.PUSH = PUSH;
     var paginaAtual = "";
-    var conteudo = JSON.stringify("visitario.json");
     document.getElementById("hospitais").addEventListener("click", function(){novaPagina("hospitais")}, false);
     document.getElementById("delegacias").addEventListener("click", function(){novaPagina("delegacias")}, false);
     document.getElementById("pontosTuristicos").addEventListener("click", function(){novaPagina("pontosTuristicos")}, false);
@@ -29,13 +28,14 @@ function pronto(){
             document.getElementById("banheiros").addEventListener("click", function(){novaPagina("banheiros")}, false);
         }
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/lista.html'){
-            alert(JSON.stringify("visitario.json").hospitais);
-            //        if (paginaAtual == "hospitais" )
-
-
-           // document.getElementById("conteudoLista").innerHTML =
+            function filtrar(key, value){
+                if (key == "hospitais"){
+                    return key;
+                }
+                return undefined;
+            }
+            document.getElementById("conteudoLista").innerHTML = JSON.stringify("visitario.json", filtrar);
         }
-
     }
 }
 
