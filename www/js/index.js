0c85dc4b9f18af89e6c70a35a7da891f755996fe
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-23 14:35:16 -0200

    v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,14 @@
 function pronto(){
     window.addEventListener('push', ratchetPronto);
     var paginaAtual = "";
-    document.getElementById("hospitais").addEventListener("click", definirPagina("hospitais"), false);
-    document.getElementById("delegacias").addEventListener("click", definirPagina("delegacias"), false);
-    document.getElementById("pontosTuristicos").addEventListener("click", definirPagina("pontosTuristicos"), false);
-    document.getElementById("praias").addEventListener("click", definirPagina("praias"), false);
-    document.getElementById("ondeComer").addEventListener("click", definirPagina("ondeComer"), false);
-    document.getElementById("ondeDormir").addEventListener("click", definirPagina("ondeDormir"), false);
-    document.getElementById("eventos").addEventListener("click", definirPagina("eventos"), false);
-    document.getElementById("banheiros").addEventListener("click", definirPagina("banheiros"), false);
-
-    function definirPagina(pagina){
-        paginaAtual = pagina;
-    }
+    document.getElementById("hospitais").addEventListener("click", function(){paginaAtual = "hospitais"}, false);
+    document.getElementById("delegacias").addEventListener("click", function(){paginaAtual ="delegacias"}, false);
+    document.getElementById("pontosTuristicos").addEventListener("click", function(){paginaAtual ="pontosTuristicos"}, false);
+    document.getElementById("praias").addEventListener("click", function(){paginaAtual ="praias"}, false);
+    document.getElementById("ondeComer").addEventListener("click", function(){paginaAtual ="ondeComer"}, false);
+    document.getElementById("ondeDormir").addEventListener("click", function(){paginaAtual ="ondeDormir"}, false);
+    document.getElementById("eventos").addEventListener("click", function(){paginaAtual ="eventos"}, false);
+    document.getElementById("banheiros").addEventListener("click", function(){paginaAtual ="banheiros"}, false);
 
     function ratchetPronto(){
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/index.html'){
